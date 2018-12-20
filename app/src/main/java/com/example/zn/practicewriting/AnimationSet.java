49ec86ef709802ba393242d130831a82e05a1b49
Capoo <flange0322@gmail.com>
2018-12-21 05:21:57 +0800

    fix the huge god code and set the anime_end pos

---
+++ app/src/main/java/com/example/zn/practicewriting/AnimationSet.java
@@ -0,0 +1,106 @@
+package com.example.zn.practicewriting;
+
+import android.animation.ObjectAnimator;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class AnimationSet {
+
+    ImageButton bt_Video;
+    ImageButton bt_Write;
+    TextView tv_Video;
+    TextView tv_Write;
+    public AnimationSet(ImageButton iv,ImageButton iw,TextView tv,TextView tw){
+        bt_Video = iv;
+        bt_Write = iw;
+        tv_Video = tv;
+        tv_Write = tw;
+    }
+
+    public void AnimationInit(){
+        ObjectAnimator tv_animator = ObjectAnimator.ofFloat(tv_Video,"alpha",0.0F,0.0F).setDuration(2000);
+        ObjectAnimator bt_animator = ObjectAnimator.ofFloat(tv_Write,"alpha",0.0F,0.0F).setDuration(2000);
+        tv_animator.start();
+        bt_animator.start();
+    }
+
+    public boolean animation_Move(Float float_x,Float float_y,String instruction){
+
+        ObjectAnimator animator;
+        ObjectAnimator animator1;
+        AnimatorEx ax = new AnimatorEx();
+        boolean moving;
+
+        switch (instruction){
+            case "Write":
+                //bt_Write_Area按鈕移動
+                animator = ObjectAnimator.ofFloat(bt_Write,"translationX",0.0F,float_x).setDuration(1000);
+                animator.start();
+                animator = ObjectAnimator.ofFloat(bt_Write,"translationY",0.0F,float_y).setDuration(1000);
+                animator.start();
+                //bt_Write_Area按鈕縮放
+                animator = ObjectAnimator.ofFloat(bt_Write,"scaleX",1.0F,1.3F).setDuration(1000);
+                animator.start();
+                animator1 = ObjectAnimator.ofFloat(bt_Write,"scaleY",1.0F,1.3F).setDuration(1000);
+                animator1.start();
+                //bt_Video_Area消失
+                animator = ObjectAnimator.ofFloat(bt_Video,"alpha",1.0F,0.0F).setDuration(1000);
+                animator.start();
+                ax.onAnimationEnd_move(animator1,tv_Write);
+                break;
+            case "Video":
+                //bt_Video_Area按鈕移動
+                animator = ObjectAnimator.ofFloat(bt_Video,"translationX",0.0F,float_x).setDuration(1000);
+                animator.start();
+                animator = ObjectAnimator.ofFloat(bt_Video,"translationY",0.0F,float_y).setDuration(1000);
+                animator.start();
+                //bt_Video_Area按鈕縮放
+                animator = ObjectAnimator.ofFloat(bt_Video,"scaleX",1.0F,1.3F).setDuration(1000);
+                animator.start();
+                animator1 = ObjectAnimator.ofFloat(bt_Video,"scaleY",1.0F,1.3F).setDuration(1000);
+                animator1.start();
+                //bt_Write_Area消失
+                animator = ObjectAnimator.ofFloat(bt_Write,"alpha",1.0F,0.0F).setDuration(1000);
+                animator.start();
+                ax.onAnimationEnd_move(animator1,tv_Video);
+                break;
+            default:
+                break;
+        }
+
+        moving = ax.getMovingStatus();
+        return moving;
+    }
+
+    public void animation_Back(Float float_x,Float float_y,String instruction){
+        ObjectAnimator animator;
+        ObjectAnimator animator1;
+        AnimatorEx ax = new AnimatorEx();
+
+        switch (instruction) {
+            case "Write":
+                //bt_Write_Area縮放
+                animator = ObjectAnimator.ofFloat(bt_Write, "scaleX", 1.3F, 1.0F).setDuration(1000);
+                animator.start();
+                animator = ObjectAnimator.ofFloat(bt_Write, "scaleY", 1.3F, 1.0F).setDuration(1000);
+                animator.start();
+                //tv_Write_Area消失
+                animator1 = ObjectAnimator.ofFloat(tv_Write, "alpha", 1.0F, 0.0F).setDuration(1000);
+                animator1.start();
+                ax.onAnimationEnd_back(animator1,float_x,float_y,bt_Write,bt_Video);
+                break;
+            case "Video":
+                animator = ObjectAnimator.ofFloat(bt_Video,"scaleX",1.3F,1.0F).setDuration(1000);
+                animator.start();
+                animator = ObjectAnimator.ofFloat(bt_Video,"scaleY",1.3F,1.0F).setDuration(1000);
+                animator.start();
+                //tv_Video_Area消失
+                animator1 = ObjectAnimator.ofFloat(tv_Video,"alpha",1.0F,0.0F).setDuration(1000);
+                animator1.start();
+                ax.onAnimationEnd_back(animator1,float_x,float_y,bt_Video,bt_Write);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/zn/practicewriting/AnimatorEx.java
@@ -0,0 +1,39 @@
+package com.example.zn.practicewriting;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class AnimatorEx extends AnimatorListenerAdapter{
+
+    boolean moving_Tmp;
+    public AnimatorEx(){
+        super();
+    }
+
+    public void onAnimationEnd_move(Animator animation,TextView tv) {
+        super.onAnimationEnd(animation);
+        ObjectAnimator animator=ObjectAnimator.ofFloat(tv,"alpha",0.0F,1.0F).setDuration(1000);
+        animator.start();
+        moving_Tmp = false;
+    }
+
+    public void onAnimationEnd_back(Animator animation, Float float_x, Float float_y, ImageButton bt_tmpA,ImageButton bt_tmpB) {
+        super.onAnimationEnd(animation);
+        ObjectAnimator animator;
+        //bt_Video_Area回原位
+        animator = ObjectAnimator.ofFloat(bt_tmpA,"translationX",float_x,0.0F).setDuration(1000);
+        animator.start();
+        animator = ObjectAnimator.ofFloat(bt_tmpA,"translationY",float_y,0.0F).setDuration(1000);
+        animator.start();
+        //bt_Write_Area出現
+        animator = ObjectAnimator.ofFloat(bt_tmpB,"alpha",0.0F,1.0F).setDuration(1000);
+        animator.start();
+    }
+
+    public boolean getMovingStatus(){
+        return moving_Tmp;
+    }
+}
